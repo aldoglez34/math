f45f7f46f185eece80b1afbcfe724d23222a69e0
Aldo Solano González <aldoglez34@gmail.com>
2020-06-16 15:20:08 -0500

    msg

--- client/src/pages/course/index.js
+++ client/src/pages/course/index.js
@@ -17,25 +17,16 @@ const Course = React.memo((props) => {
   };
 
   // useEffect(() => {
-  //   var editor;
-  //   window.onload = function () {
-  //     editor = com.wiris.jsEditor.JsEditor.newInstance({ language: "en" });
-  //     editor.insertInto(document.getElementById("editorContainer"));
-  //   };
+  //   // var editor = com.wiris.jsEditor.JsEditor.newInstance({ language: "es" });
+  //   // console.log("editor", editor);
   // }, []);
-
-  // var hand;
-  // window.onload = function () {
-  //   hand = com.wiris.js.JsHand.newInstance();
-  //   hand.insertInto(document.getElementById("handContainer"));
-  // };
 
   return (
     <Layout>
       <StudentNav title={"Mis Cursos // " + getCourse()} hasExitBttn={true} />
       <Container className="py-4 px-2 mb-3 px-lg-0">
         <h3>Math type test:</h3>
-        {/* <div id="editorContainer"></div> */}
+        <div id="editorContainer"></div>
       </Container>
     </Layout>
   );
