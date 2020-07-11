a2d8e3b5ae02a72c1adb9469bc3dd7e9a71a62bc
Aldo Solano González <aldoglez34@gmail.com>
2020-07-11 17:31:33 -0500

    msg

---
+++ client/build/precache-manifest.479eb5a2b4d690fb25260bfb799e03b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebfb60313719a23d24838dc33bfd4b56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96f0fe309d1fe71f6353",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d1ac17e06223310e6f1a",
+    "url": "/static/css/main.ccb6a8d8.chunk.css"
+  },
+  {
+    "revision": "96f0fe309d1fe71f6353",
+    "url": "/static/js/2.1eb8be23.chunk.js"
+  },
+  {
+    "revision": "0afcbd5035c82e37893809bfa776589a",
+    "url": "/static/js/2.1eb8be23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1ac17e06223310e6f1a",
+    "url": "/static/js/main.6a19da13.chunk.js"
+  },
+  {
+    "revision": "d8f8e2bb6e8daeaf6626",
+    "url": "/static/js/runtime-main.54b2b051.js"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7c71f06e7cfe8ca7a2d47faed6f7cec3.js"
+  "/precache-manifest.479eb5a2b4d690fb25260bfb799e03b8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
