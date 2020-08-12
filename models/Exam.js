ab71af2984784e57f71f15d3c0124320eb4816cd
Aldo Solano González <aldoglez34@gmail.com>
2020-08-11 20:27:57 -0500

    msg

--- models/Exam.js
+++ models/Exam.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
+  topicCode: { type: String, required: true }, // ej. PRIM3y4_Suma o PRIM3y4_Resta
   name: {
     type: String,
     required: true,
   }, // ej. Sumas 1 o Restas 3
-  topicCode: { type: String, required: true }, // ej. PRIM3y4_Suma o PRIM3y4_Resta
   topicName: { type: String, required: true }, // ej. Suma, Resta, etc
   description: { type: String, required: true }, // ej. "Este es un examen blah blah..."
   duration: { type: Number, required: true }, // ej. 30 (mins)
