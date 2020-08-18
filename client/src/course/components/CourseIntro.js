99633ca1de444667bac6a6d018f02f890bd8fab8
Aldo Solano <aldoglez34@gmail.com>
2020-08-18 00:48:33 -0500

    msg

--- client/src/course/components/CourseIntro.js
+++ client/src/course/components/CourseIntro.js
@@ -21,7 +21,7 @@ const CourseIntro = React.memo(({ name, topics, rewards }) => {
         <Row>
           {/* temario */}
           <Col lg={6}>
-            <h3 style={{ color: "#828c90" }} className="mb-2">
+            <h3 style={{ color: "#828c90" }} className="mb-3">
               Temario
             </h3>
             {/* <br /> */}
@@ -33,7 +33,7 @@ const CourseIntro = React.memo(({ name, topics, rewards }) => {
           </Col>
           {/* medallero */}
           <Col lg={6} className="mt-4 mt-lg-0 pb-3 pb-lg-0">
-            <h3 style={{ color: "#828c90" }} className="mb-2">
+            <h3 style={{ color: "#828c90" }} className="mb-3">
               Medallero
             </h3>
             {/* <br /> */}
