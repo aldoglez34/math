0326ab2823c3d9f0b28b020f29db0a6324b8c017
Aldo Solano González <aldoglez34@gmail.com>
2020-12-03 19:58:30 -0600

    msg

--- client/src/pages/landingpage/index.js
+++ client/src/pages/landingpage/index.js
@@ -17,7 +17,6 @@ const Home = React.memo(() => {
       <TeacherJumbotron />
       <FacilitiesJumbotron />
       <FAQJumbotron />
-      testtttt
       <ScrollButton scrollStepInPx={150} delayInMs={16.66} />
     </Layout>
   );
