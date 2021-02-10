5b0998eedcc2a876d901f79b08289797f752fd7a
Aldo Solano <aldoglez34@gmail.com>
2021-02-10 00:23:56 -0600

    msg

--- client/src/adminpages/topicdetail/components/DraggableMaterial.jsx
+++ client/src/adminpages/topicdetail/components/DraggableMaterial.jsx
@@ -3,6 +3,7 @@ import { array, func, string } from "prop-types";
 import { ReactSortable } from "react-sortablejs";
 import { AdminDangerButton } from "../../components";
 import TeacherAPI from "../../../utils/TeacherAPI";
+import { Button, Modal } from "react-bootstrap";
 
 import styles from "./draggablematerial.module.scss";
 
@@ -13,6 +14,15 @@ export const DraggableMaterial = ({
   topicId,
 }) => {
   const [state, setState] = useState(material);
+  const [showDeleteConfirm, setShowDeleteConfirm] = useState(false);
+
+  const [materialToDelete, setMaterialToDelete] = useState(false);
+
+  const handleShowDeleteConfirm = (m) => {
+    setMaterialToDelete(m);
+    setShowDeleteConfirm(true);
+  };
+  const handleCloseDeleteConfirm = () => setShowDeleteConfirm(false);
 
   useEffect(() => {
     const changes = state.reduce((acc, cv, idx) => {
@@ -35,26 +45,55 @@ export const DraggableMaterial = ({
         });
   }, [courseId, state, topicId]);
 
-  return state.length ? (
-    <ul className="mb-1">
-      <ReactSortable list={state} setList={setState}>
-        {state.map((m) => (
-          <li key={m._id}>
-            <strong className={styles.item}>
-              {m.type === "video" && <i className="fas fa-video mr-2" />}
-              {m.type === "pdf" && <i className="fas fa-file-pdf mr-2" />}
-              {m.name}
-              <AdminDangerButton
-                icon={<i className="fas fa-times" />}
-                onClick={() => handleDeleteMaterialItem(m.type, m._id, m.link)}
-              />
-            </strong>
-          </li>
-        ))}
-      </ReactSortable>
-    </ul>
-  ) : (
-    <h5>-</h5>
+  return (
+    <React.Fragment>
+      <Modal show={showDeleteConfirm} onHide={handleCloseDeleteConfirm}>
+        <Modal.Body>
+          {`¿Estás seguro que quieres borrar este ${materialToDelete.type}?`}
+          <br />
+          <div className="text-center mt-4">
+            <Button
+              variant="danger"
+              className="mr-2"
+              onClick={() =>
+                handleDeleteMaterialItem(
+                  materialToDelete.type,
+                  materialToDelete._id,
+                  materialToDelete.link
+                )
+              }
+            >
+              Borrar
+            </Button>
+            <Button variant="dark" onClick={handleCloseDeleteConfirm}>
+              Borrar
+            </Button>
+          </div>
+        </Modal.Body>
+      </Modal>
+
+      {state.length ? (
+        <ul className="mb-1">
+          <ReactSortable list={state} setList={setState}>
+            {state.map((m) => (
+              <li key={m._id}>
+                <strong className={styles.item}>
+                  {m.type === "video" && <i className="fas fa-video mr-2" />}
+                  {m.type === "pdf" && <i className="fas fa-file-pdf mr-2" />}
+                  {m.name}
+                  <AdminDangerButton
+                    icon={<i className="fas fa-times" />}
+                    onClick={(m) => handleShowDeleteConfirm(m)}
+                  />
+                </strong>
+              </li>
+            ))}
+          </ReactSortable>
+        </ul>
+      ) : (
+        <h5>-</h5>
+      )}
+    </React.Fragment>
   );
 };
 
