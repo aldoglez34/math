7ac1bf50822a06de48354e4c0824f7164a238457
Aldo Solano González <aldoglez34@gmail.com>
2021-02-18 16:03:15 -0600

    msg

--- client/src/components/navbar/MyNavbar.jsx
+++ client/src/components/navbar/MyNavbar.jsx
@@ -2,58 +2,55 @@ import React from "react";
 import { Navbar, Nav, Col, Container } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { CoursesDropdown, StudentDropdown } from "./components";
-import "./navbar.scss";
+import cn from "classnames";
 
-export const MyNavbar = React.memo(() => {
+import styles from "./navbar.module.scss";
+
+export const MyNavbar = () => {
   const student = useSelector((state) => state.student);
-
   const zenMode = useSelector((state) => state.zenMode);
+
+  const Logo = () => {
+    return zenMode ? (
+      <h2 className={cn("mb-0", styles.logo, styles.zen)}>
+        <s>MeXmáticas</s>
+        <small className={styles.beta}>v1.2</small>
+      </h2>
+    ) : (
+      <h2 className={cn("mb-0", styles.logo)}>
+        MeXmáticas<small className={styles.beta}>v1.2</small>
+      </h2>
+    );
+  };
 
   return (
     <Navbar
-      fixed="top"
+      className={cn("py-2", "shadow", styles.container)}
       collapseOnSelect
       expand="lg"
-      id="mynavbar"
+      fixed="top"
       variant="dark"
-      className="py-2 shadow"
     >
-      {zenMode ? (
-        <Navbar.Brand className="d-block d-lg-none">
-          <h2 className="mb-0 mainLogo zenLogo">
-            <s>MeXmáticas</s>
-          </h2>
-        </Navbar.Brand>
-      ) : (
-        <Navbar.Brand href="/" className="d-block d-lg-none">
-          <h2 className="mb-0 mainLogo">MeXmáticas</h2>
-        </Navbar.Brand>
-      )}
+      <Navbar.Brand className="d-block d-lg-none">
+        <Logo />
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
       <Navbar.Collapse id="responsive-navbar-nav">
         <Container className="px-0 px-lg-3">
-          {/* left */}
+          {/* LEFT NAV */}
           <Nav as={Col} className="d-none d-lg-block pr-0">
             <div className="d-flex flex-row">
               <CoursesDropdown />
             </div>
           </Nav>
-          {/* middle */}
+          {/* MIDDLE NAV */}
           <Nav as={Col} className="d-none d-lg-block text-center pr-0">
-            {zenMode ? (
-              <Navbar.Brand>
-                <h2 className="mb-0 mainLogo zenLogo">
-                  <s>MeXmáticas</s>
-                </h2>
-              </Navbar.Brand>
-            ) : (
-              <Navbar.Brand href="/">
-                <h2 className="mb-0 mainLogo">MeXmáticas</h2>
-              </Navbar.Brand>
-            )}
+            <Navbar.Brand>
+              <Logo />
+            </Navbar.Brand>
           </Nav>
-          {/* right */}
+          {/* RIGHT NAV */}
           <Nav
             as={Col}
             className="justify-content-end align-items-lg-end mt-2 mt-lg-0 pr-0"
@@ -83,10 +80,13 @@ export const MyNavbar = React.memo(() => {
               </>
             ) : (
               <>
-                <Nav.Link className="navbarItem mr-lg-4" href="/login">
+                <Nav.Link
+                  className={cn("mr-lg-4", styles.navbarItem)}
+                  href="/login"
+                >
                   Inicia Sesión
                 </Nav.Link>
-                <Nav.Link className="navbarItem" href="/signup">
+                <Nav.Link className={styles.navbarItem} href="/signup">
                   Regístrate
                 </Nav.Link>
                 <Nav className="d-block d-lg-none">
@@ -99,4 +99,4 @@ export const MyNavbar = React.memo(() => {
       </Navbar.Collapse>
     </Navbar>
   );
-});
+};
